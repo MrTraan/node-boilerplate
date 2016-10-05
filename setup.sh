8d16e099d019ba60a7ba153653152c8ca127659c
Nathan Grasset <ngrasset@student.42.fr>
2016-10-05 16:57:34 +0200

    first commit

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+npm install --save-dev	babel-cli \
+						babel-eslint \
+						babel-preset-es2015 \
+						babel-preset-stage-0 \
+						chai \
+						eslint \
+						eslint-config-airbnb \
+						eslint-plugin-import \
+						eslint-plugin-jsx-a11y \
+						eslint-plugin-react \
+						mocha
