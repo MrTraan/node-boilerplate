9e53fca5737fa902c88ae2da01b4f5b375ae8356
Nathan Grasset <ngrasset@student.42.fr>
2017-01-09 12:19:12 +0100

    update boilerplate

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 npm install --save-dev	babel-cli \
-						babel-eslint \
-						babel-preset-es2015 \
-						babel-preset-stage-0 \
-						chai \
-						eslint \
-						eslint-config-airbnb \
-						eslint-plugin-import \
-						eslint-plugin-jsx-a11y \
-						eslint-plugin-react \
-						mocha
+	babel-eslint \
+	babel-preset-es2015 \
+	babel-preset-stage-0 \
+	babel-plugin-add-module-exports \
+	babel-plugin-transform-builtin-extend \
+	babel-plugin-transform-runtime \
+	chai \
+	eslint \
+	eslint-config-airbnb \
+	eslint-plugin-import \
+	eslint-plugin-jsx-a11y \
+	eslint-plugin-react \
+	mocha
